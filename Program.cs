adea003124c58a6d3292aca3429a2c473faaa100
Nayemsyed <sdnayem04112000@gmail.com>
2024-02-16 10:12:49 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CalcExample;
+Calc calc=new Calc();
+Console.WriteLine("Operations On Numbers");
+calc.Add(15, 25);
+calc.Sub(90, 25);
+calc.Multi(15, 25);
+calc.Div(50, 25);
+
